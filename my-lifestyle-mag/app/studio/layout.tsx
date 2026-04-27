e634af3e2f764a82fbae8e48d0f4401e90806748
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-04-27 21:49:53 +0000

    kn ñm m

---
+++ my-lifestyle-mag/app/studio/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Vanguard Studio',
+  description: 'Sanity Studio Configuration',
+}
+
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
