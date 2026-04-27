e41294b3cba38e5e1e5d291152d197297dd812f2
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-04-27 22:11:18 +0000

    mn

--- my-lifestyle-mag/sanity.config.ts
+++ my-lifestyle-mag/sanity.config.ts
@@ -6,9 +6,10 @@ export default defineConfig({
   name: 'vanguard-studio',
   title: 'Vanguard Lifestyle Magazine',
   
-  // Usamos as string para asegurar a TypeScript que estos valores existirán
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+  // Tu ID real según el error que me has pasado
+  projectId: 'fpdiv8gk', 
+  // El nombre estándar del dataset (donde vive la info)
+  dataset: 'production',
 
   basePath: '/studio',
 
