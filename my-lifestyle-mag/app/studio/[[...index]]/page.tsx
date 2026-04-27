83e6ddae37c1b0e148fff841508a9dfa1daec8e9
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-04-27 21:50:42 +0000

    mln

--- my-lifestyle-mag/app/studio/[[...index]]/page.tsx
+++ my-lifestyle-mag/app/studio/[[...index]]/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { NextStudio } from "next-sanity/studio";
+import config from "../../../sanity.config";
 
-// Vanguard Fix: Adiós a los saltos de carpeta. 
-// Usamos la importación dinámica y absoluta.
-import config from "../../../sanity.config"; 
-// (Nota: Si el config está en la raíz estricta junto a package.json, 
-// a veces el alias @/ no lo atrapa dependiendo de tu tsconfig. 
-// Si la ruta relativa sigue fallando, prueba a mover sanity.config.ts 
-// dentro de una carpeta /sanity y actualiza la ruta).
+export default function StudioPage() {
+  return <NextStudio config={config} />;
+}
