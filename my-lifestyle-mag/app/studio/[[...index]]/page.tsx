3b6ab2c49a3584e917eaaa47a68432c61bac1bde
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-04-27 20:23:09 +0000

    mn

--- my-lifestyle-mag/app/studio/[[...index]]/page.tsx
+++ my-lifestyle-mag/app/studio/[[...index]]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { NextStudio } from "next-sanity/studio";
-import config from "../../../../sanity.config";
+// El Fix: Tres saltos exactos hacia la raíz de Next.js
+import config from "../../../sanity.config";
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
