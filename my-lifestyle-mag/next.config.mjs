ab68ab7c36533100d96a342057c3c1044a86f55d
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-04-27 21:36:40 +0000

    knkñ

--- my-lifestyle-mag/next.config.mjs
+++ my-lifestyle-mag/next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Esto permite que el despliegue termine aunque haya avisos de ESLint (como el de 'any')
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Esto permite que el despliegue termine aunque haya errores de tipos de TypeScript
     ignoreBuildErrors: true,
   },
 };
